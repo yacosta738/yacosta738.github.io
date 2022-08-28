bb1e57567395483a433c40e5011ee0ee7669a8d5
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2022-08-28 14:14:46 -0300

    add contact section 🛂

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+    readonly FORMSPREE: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
