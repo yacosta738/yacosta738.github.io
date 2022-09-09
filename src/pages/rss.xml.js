9059b5cf35ba4cde56ef719579dbe6ff8df834bd
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2022-09-09 17:39:32 -0300

    Add rss support

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,21 @@
 import rss from '@astrojs/rss'
 
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true })
+const posts = Object.values(postImportResult)
+
 export const get = () =>
 	rss({
 		title: 'YAP’s  Blog',
-		description: 'A humble Astronaut’s guide to the stars',
+		description:
+			'Blog about programming and web technologies, scalable, high availability and tips to be more productive.',
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.md'),
+		items: posts
+			.filter((post) => !post.frontmatter.draft)
+			.map((post) => ({
+				link: post.url,
+				title: post.frontmatter.title,
+				pubDate: new Date(post.frontmatter.date),
+				description: post.frontmatter.description
+			})),
 		stylesheet: '/rss/styles.xsl'
 	})
