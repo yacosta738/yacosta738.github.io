053097388b8435bbc953a3cd8bbce63e4e826452
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2022-09-18 16:20:00 -0300

    Update blog.spec.ts

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -4,7 +4,7 @@ test('test blog section', async ({ page }) => {
 	await page.goto('http://localhost:3000/')
 
 	await page.locator('text=Blog').nth(1).click()
-	await expect(page).toHaveURL('http://localhost:3000/blog')
+	await expect(page).toHaveURL('http://localhost:3000/blog/')
 
 	await page.locator('[data-test="paginator-next"]').click()
 	await expect(page).toHaveURL('http://localhost:3000/blog/2')
@@ -12,5 +12,5 @@ test('test blog section', async ({ page }) => {
 	await page.goto('http://localhost:3000/blog/rebuilding-site-with-astro/')
 
 	await page.locator('text=Back to Blog').click()
-	await expect(page).toHaveURL('http://localhost:3000/blog')
+	await expect(page).toHaveURL('http://localhost:3000/blog/')
 })
