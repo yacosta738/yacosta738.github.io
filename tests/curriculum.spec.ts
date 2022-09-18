e2dfcfcf163be89a9a4e611880c69925f6e0e54f
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2022-09-18 14:12:13 -0300

    fixed some tests

--- tests/curriculum.spec.ts
+++ tests/curriculum.spec.ts
@@ -3,9 +3,13 @@ import { test } from '@playwright/test'
 test('test resume button 📓', async ({ page }) => {
 	await page.goto('http://localhost:3000/')
 
-	const [page1] = await Promise.all([
+	const [curriculum] = await Promise.all([
 		page.waitForEvent('popup'),
 		page.locator('text=Resume').nth(1).click()
 	])
-  await page1.goto('http://localhost:3000/files/yuniel_acosta_cv.pdf')
+
+	await curriculum.goto('https://github.com/yacosta738')
+	await curriculum.goto('https://www.linkedin.com/in/yacosta738/')
+	await curriculum.goto('https://www.yunielacosta.com/')
+	await curriculum.close()
 })
