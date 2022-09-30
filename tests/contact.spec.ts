9b14d6916483a7fc018245d75382ab0732d7ab1a
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2022-09-30 12:35:24 -0300

    fixed text in placeholder and add tests e2e with id

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -3,23 +3,23 @@ import { test, expect } from '@playwright/test'
 test('test contact section', async ({ page }) => {
 	await page.goto('http://localhost:3000/')
 
-	await page.locator('[placeholder="John Doe"]').click()
+	await page.locator('[id="form-name"]').click()
 
-	await page.locator('[placeholder="John Doe"]').fill('Yuniel Acosta')
+	await page.locator('[id="form-name"]').fill('Yuniel Acosta')
 
-	await page.locator('[placeholder="jhondoe\\@gmail\\.com"]').click()
+	await page.locator('[id="form-email"]').click()
 
-	await page.locator('[placeholder="jhondoe\\@gmail\\.com"]').fill('yunielacosta738@gmail.com')
+	await page.locator('[id="form-email"]').fill('yunielacosta738@gmail.com')
 
-	await page.locator('[placeholder="John it\\\'s time to start your new project"]').click()
+	await page.locator('[id="form-subject"]').click()
+
+	await page.locator('[id="form-subject"]').fill('I want contact you')
+
+	await page.locator('textarea[id="form-message"]').click()
 
 	await page
-		.locator('[placeholder="John it\\\'s time to start your new project"]')
-		.fill('I want contact you')
-
-	await page.locator('textarea[name="message"]').click()
-
-	await page.locator('textarea[name="message"]').fill('I think that you and me could work together')
+		.locator('textarea[id="form-message"]')
+		.fill('I think that you and me could work together')
 
 	await page.locator('text=Send Message').click()
 	await expect(page).toHaveURL('https://formspree.io/f/xknperzd')
