9c5ce3d636915538555c61a4a217cf796fce9b3b
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2022-12-15 11:40:49 -0300

    master < Feature/add comment support (#252)
    * add comment support
    
    * Update pnpm-lock.yaml
    
    * feat(blog) fixed post header article issues
    
    add json to article
    
    * Update Article.ts

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -16,19 +16,26 @@ export interface Article {
 }
 
 export const jsonToArticle = (json: any): Article => {
+	const frontmatter = json.frontmatter
+	let content = ''
+	// check if json has rawContent()
+	// if it does, it means it's a markdown file
+	if (json.rawContent) {
+		content = json.rawContent()
+	}
 	return {
 		id: json.id || uuidv4(),
 		url: json.url,
-		title: json.frontmatter.title,
-		description: json.frontmatter.description,
-		date: json.frontmatter.date,
-		cover: json.frontmatter.cover,
-		author: json.frontmatter.author,
-		timeToRead: json.frontmatter.minutesRead,
-		lang: json.frontmatter.lang,
-		tags: json.frontmatter.tags,
-		categories: json.frontmatter.categories,
-		draft: json.frontmatter.draft,
-		content: json.rawContent()
+		title: frontmatter?.title || json.title,
+		description: frontmatter?.description || json.description,
+		date: frontmatter?.date || json.date,
+		cover: frontmatter?.cover || json.cover,
+		author: frontmatter?.author || json.author,
+		timeToRead: frontmatter?.minutesRead || json.timeToRead,
+		lang: frontmatter?.lang || json.lang,
+		tags: frontmatter?.tags || json.tags,
+		categories: frontmatter?.categories || json.categories,
+		draft: frontmatter?.draft || json.draft,
+		content
 	}
 }
