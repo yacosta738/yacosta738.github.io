6620c73c0a4758c751c29682b5d788127febfd8d
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-03-08 10:09:54 -0300

    fix(blog): :ambulance: remove remark plugin
    remove rehypeAccessibleEmojis plugin

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,6 @@ import vue from '@astrojs/vue'
 import robotsTxt from 'astro-robots-txt'
 import NetlifyCMS from 'astro-netlify-cms'
 import remarkToc from 'remark-toc'
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
 
 import compress from 'astro-compress'
 
@@ -319,7 +318,7 @@ export default defineConfig({
 		plugins: []
 	},
 	markdown: {
-		remarkPlugins: [remarkToc, rehypeAccessibleEmojis, remarkReadingTime],
+		remarkPlugins: [remarkToc, remarkReadingTime],
 		gfm: true
 	}
 })
