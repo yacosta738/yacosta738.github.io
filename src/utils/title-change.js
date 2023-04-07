b8eb20c2d44a9e8513a0d77ce49d47a5abb2ade6
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-04-07 14:04:53 +0200

    feat(core) :twisted_rightwards_arrows: add delay to title change
    delay 10 seconds title change
    
    Closed #624

--- src/utils/title-change.js
+++ src/utils/title-change.js
@@ -1,11 +1,28 @@
-let previousTitle = document.title
-	let titleTextAlt = { en: "Don't go! Come back! 😥", es: '¡No te vayas! ¡Vuelve! 😥' }
+// Store the current document title in a variable
+let previousTitle = document.title;
 
-	window.addEventListener('blur', () => {
-		previousTitle = document.title
-		document.title = titleTextAlt[document.documentElement.lang]
-	})
+// Define alternative title text for each language
+let titleTextAlt = { en: "Don't go! Come back! 😥", es: '¡No te vayas! ¡Vuelve! 😥' };
 
-	window.addEventListener('focus', () => {
-		document.title = previousTitle
-	})
+// Declare a variable to hold the ID of the timeout task
+let timeoutId;
+
+// When the window loses focus (e.g. the user switches to another tab)
+window.addEventListener('blur', () => {
+  // Cancel the previous timeout task if it's still running
+  clearTimeout(timeoutId);
+  // Store the current title in the previousTitle variable
+  previousTitle = document.title;
+  // Schedule a new timeout task that changes the title after 10 seconds
+  timeoutId = setTimeout(() => {
+    document.title = titleTextAlt[document.documentElement.lang];
+  }, 10000);
+});
+
+// When the window gains focus again (e.g. the user comes back to the tab)
+window.addEventListener('focus', () => {
+  // Cancel the previous timeout task if it's still running
+  clearTimeout(timeoutId);
+  // Restore the previous title
+  document.title = previousTitle;
+});
