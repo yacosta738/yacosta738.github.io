8254d9d0ce2d93e55b213974c7638c6fd1c04674
Yuniel Acosta <yunielacosta738@gmail.com>
2023-04-12 07:53:25 +0000

    feat(jobs) :zap: add support for job timeline #623

--- src/models/Job.ts
+++ src/models/Job.ts
@@ -1,19 +1,19 @@
 export interface Role {
-  role?: string;
-  startDate?: Date | string;
-  endDate?: Date | string;
-  achievements?: string[];
+	role?: string
+	startDate?: Date | string
+	endDate?: Date | string
+	achievements?: string[]
 }
 
 export interface Job {
-  id?: string;
-  title?: string;
-  lang?: string;
-  company?: string;
-  icon?: string;
-  location?: string;
-  url?: string;
-  published: boolean;
-  roles?: Role[];
-	createDate?: Date | string;
+	id?: string
+	title?: string
+	lang?: string
+	company?: string
+	icon?: string
+	location?: string
+	url?: string
+	published: boolean
+	roles?: Role[]
+	createDate?: Date | string
 }
