c94f71270ef31409bdcacdc425581865e7e6edec
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-04-21 13:34:22 +0200

    fix(core) fix pwa

--- astro.config.ts
+++ astro.config.ts
@@ -17,28 +17,28 @@ export default defineConfig({
 	site: 'https://yunielacosta.com/',
 	integrations: [
 		AstroPWA({
-			mode: 'development',
+			mode: 'production',
 			base: '/',
 			scope: '/',
-			includeAssets: ['favicon.svg'],
+			includeAssets: ['favicons/favicon.ico'],
 			registerType: 'autoUpdate',
 			manifest: {
-				name: 'Astro PWA',
-				short_name: 'Astro PWA',
-				theme_color: '#ffffff',
+				name: 'Yuniel Acosta',
+				short_name: 'YAP',
+				theme_color: '#64ffda',
 				icons: [
 					{
-						src: 'pwa-192x192.png',
+						src: 'android-chrome-192x192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: 'pwa-512x512.png',
+						src: '/android-chrome-512x512.png',
 						sizes: '512x512',
 						type: 'image/png'
 					},
 					{
-						src: 'pwa-512x512.png',
+						src: '/android-chrome-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any maskable'
