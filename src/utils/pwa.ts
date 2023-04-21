82ee005add17000dd268abcecae7473a0ca1e9fe
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-04-21 10:45:31 +0200

    feat(core) add pwa support

--- src/utils/pwa.ts
+++ src/utils/pwa.ts
@@ -1,27 +1,13 @@
-// Type imports
-import type { ManifestOptions } from 'vite-plugin-pwa'
+import { registerSW } from 'virtual:pwa-register'
 
-/**
- * Defines the configuration for PWA webmanifest.
- */
-export const manifest: Partial<ManifestOptions> = {
-	name: 'Yuniel Acosta',
-	short_name: 'yap',
-	description:
-		'Programmer, writer, technology and science enthusiast, specialized in building web applications. Interested in Vue, Typescript, Node.js, Java/Kotlin and Spring Boot.',
-	theme_color: '#64ffda',
-	background_color: '#0a192f',
-	display: 'minimal-ui',
-	icons: [
-		{
-			src: 'android-chrome-192x192.png',
-			sizes: '192x192',
-			type: 'image/png'
-		},
-		{
-			src: '/android-chrome-512x512.png',
-			sizes: '512x512',
-			type: 'image/png'
-		}
-	]
-}
+registerSW({
+	immediate: true,
+	onRegisteredSW (swScriptUrl) {
+		// eslint-disable-next-line no-console
+		console.log('SW registered: ', swScriptUrl)
+	},
+	onOfflineReady () {
+		// eslint-disable-next-line no-console
+		console.log('PWA application ready to work offline')
+	}
+})
