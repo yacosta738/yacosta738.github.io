366bcabceadefb20c9acf48b68eb46f5a0cf66a5
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-04-27 21:57:42 +0200

    fix(core) fixed issue with pwa
    Removed duplicated code ✂️
    
    fixed issues with sw 👷‍♂️

--- tests/integration/blog.spec.ts
+++ tests/integration/blog.spec.ts
@@ -4,7 +4,7 @@ test('test blog section', async ({ page }) => {
 	await page.goto('http://localhost:3000/')
 
 	await page.locator('text=Blog').nth(1).click()
-	await expect(page).toHaveURL('http://localhost:3000/blog/')
+	await expect(page).toHaveURL('http://localhost:3000/blog')
 
 	await page.locator('[data-test="paginator-next"]').click()
 	await expect(page).toHaveURL('http://localhost:3000/blog/2')
