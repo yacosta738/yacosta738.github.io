77059939124a78423a297a7933ea4bea66ccebbb
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-09-20 17:03:30 +0200

    chore(deps): Updated and migrated libs

--- .astro/icon.d.ts
+++ .astro/icon.d.ts
@@ -37707,6 +37707,10 @@ declare module 'astro-icon' {
 		| "akar-icons:youtube-fill"
 		| "akar-icons:zoom-fill"
 		| "akar-icons:zoom-in"
-		| "akar-icons:zoom-out";
+		| "akar-icons:zoom-out"
+		| "desoft"
+		| "gft"
+		| "meli"
+		| "scanntech";
 
   }
