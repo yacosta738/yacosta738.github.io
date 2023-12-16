b1e18df485cb0db4bba13d53147152f01a89c82b
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-12-16 19:47:59 +0100

    Refactor language tag normalization and translation functions

--- src/i18n/bcp-normalize.ts
+++ src/i18n/bcp-normalize.ts
@@ -5,7 +5,7 @@
  * @param tag Language tag to normalize, e.g. `pt-br` → `pt-BR`
  */
 
-export function normalizeLangTag(tag: string) {
+export function normalizeLangTag (tag: string) {
 	if (!tag.includes('-')) return tag.toLowerCase()
 	const [lang, region] = tag.split('-')
 	return lang.toLowerCase() + '-' + region.toUpperCase()
