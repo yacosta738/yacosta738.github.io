873db4a3ddf2cc197b7417e2987385f95abae18e
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-12-24 16:11:50 +0100

    Update search-blog.spec.ts

--- tests/integration/search-blog.spec.ts
+++ tests/integration/search-blog.spec.ts
@@ -1,16 +1,17 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-// test('test search box in blog', async ({ page }) => {
-// 	await page.goto('http://localhost:3000/blog');
-// 	await page.locator('#acosta-navbar').getByRole('button').click();
-// 	await page.getByPlaceholder('Search...').click();
-// 	await page.getByPlaceholder('Search...').fill('vuejs');
-// 	// get text of #itemFoundText and check if it is equal to '1 item found'
-// 	await page.waitForSelector('#itemFoundText');
-// 	const itemFoundText = await page.$eval('#itemFoundText', (el) => el.textContent);
-// 	expect(itemFoundText).toBeDefined();
-// 	await page.getByPlaceholder('Search...').click();
-// 	await page.getByPlaceholder('Search...').fill('kotlin');
-// 	await page.getByRole('button', { name: 'Close modal' }).click();
-// 	await page.getByRole('link', { name: 'yuniel acosta portfolio logo' }).click();
-// });
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/blog');
+  await page.getByRole('button', { name: 'Search' }).click();
+
+	const noResults = await page.getByText('No recent searches');
+  const esc = await page.getByText('Esc to close');
+	expect(noResults).toBeDefined();
+	expect(esc).toBeDefined();
+
+  await page.getByPlaceholder('Search articles and').fill('vuejs');
+  await page.locator('#results div').filter({ hasText: 'FREE HOSTINGS for WEB' }).first().click();
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.getByPlaceholder('Search articles and').fill('angular');
+  await page.getByPlaceholder('Search articles and').press('Escape');
+});
