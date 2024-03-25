414987d03a7d9cc0dcc13ac62f72a5e74228e52b
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2024-03-25 11:03:25 +0100

    fix: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@
 import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	test: {
 		/* for example, use global to avoid globals imports (describe, test, expect): */
