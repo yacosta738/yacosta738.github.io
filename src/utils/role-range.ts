4169964005fa24591b4172692dbe3342231e90ad
yacosta738 <yacosta738@users.noreply.github.com>
2024-10-08 20:56:20 +0000

    [ci] format

--- src/utils/role-range.ts
+++ src/utils/role-range.ts
@@ -3,6 +3,6 @@ import type { IRole } from '../models/Job';
 export const range = (rol?: IRole, present?: string): string => {
 	const date = rol?.startDate ? new Date(rol.startDate) : new Date();
 	return `${date.toDateString()} - ${
-		rol?.endDate ? new Date(rol.endDate).toDateString() : present ?? 'Present'
+		rol?.endDate ? new Date(rol.endDate).toDateString() : (present ?? 'Present')
 	}`;
 };
