ac7f182e6859e500231f15946ad545bfef97f8c9
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-01-18 00:49:40 +0100

    refactor: update contact form integration and improve test cases

--- tests/integration/contact.spec.ts
+++ tests/integration/contact.spec.ts
@@ -3,24 +3,29 @@ import { test, expect } from '@playwright/test';
 test('test contact section', async ({ page }) => {
 	await page.goto('http://localhost:4321');
 
+	await page.routeFromHAR('./hars/contact.har', {
+    url: '*/**/api/v1/fruits',
+    update: true,
+  });
+
 	await page.locator('[id="form-name"]').click();
 
-	await page.locator('[id="form-name"]').fill('Yuniel Acosta');
+	await page.locator('[id="form-name"]').fill('John Doe');
 
 	await page.locator('[id="form-email"]').click();
 
-	await page.locator('[id="form-email"]').fill('yunielacosta738@gmail.com');
+	await page.locator('[id="form-email"]').fill('johndoe@test.com');
 
 	await page.locator('[id="form-subject"]').click();
 
-	await page.locator('[id="form-subject"]').fill('I want contact you');
+	await page.locator('[id="form-subject"]').fill('John it is time to start your new project');
 
 	await page.locator('textarea[id="form-message"]').click();
 
 	await page
 		.locator('textarea[id="form-message"]')
-		.fill('I think that you and me could work together');
+		.fill('I think that success depends on communication and focus on the goal. I would like to work with you on your project.');
 
 	await page.locator('text=Send Message').click();
-	await expect(page).toHaveURL('https://formspree.io/f/xknperzd');
+	await expect(page).toHaveURL('https://n8n-k4aj.onrender.com/webhook-test/8901e5dd-9459-44df-86b7-8657178868f5');
 });
