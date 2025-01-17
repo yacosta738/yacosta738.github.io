f4f38002b0096866861de2889f84e765b60953c2
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-01-18 00:49:58 +0100

    refactor: format HAR files and update integration tests for consistency

--- tests/integration/contact.spec.ts
+++ tests/integration/contact.spec.ts
@@ -4,9 +4,9 @@ test('test contact section', async ({ page }) => {
 	await page.goto('http://localhost:4321');
 
 	await page.routeFromHAR('./hars/contact.har', {
-    url: '*/**/api/v1/fruits',
-    update: true,
-  });
+		url: '*/**/api/v1/fruits',
+		update: true,
+	});
 
 	await page.locator('[id="form-name"]').click();
 
@@ -24,8 +24,12 @@ test('test contact section', async ({ page }) => {
 
 	await page
 		.locator('textarea[id="form-message"]')
-		.fill('I think that success depends on communication and focus on the goal. I would like to work with you on your project.');
+		.fill(
+			'I think that success depends on communication and focus on the goal. I would like to work with you on your project.'
+		);
 
 	await page.locator('text=Send Message').click();
-	await expect(page).toHaveURL('https://n8n-k4aj.onrender.com/webhook-test/8901e5dd-9459-44df-86b7-8657178868f5');
+	await expect(page).toHaveURL(
+		'https://n8n-k4aj.onrender.com/webhook-test/8901e5dd-9459-44df-86b7-8657178868f5'
+	);
 });
